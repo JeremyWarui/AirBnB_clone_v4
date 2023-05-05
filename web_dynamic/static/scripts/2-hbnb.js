d0363957e0acfaf186b75426a238c13f66777ef6
jdev <20902932+osekojoe@users.noreply.github.com>
2023-05-05 10:14:03 +0200

    API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+// This script should be executed only when the DOM is loaded
+$(document).ready(function () {
+
+  // Initialize an empty object to store the selected amenities
+  const amenities = {};
+
+  // Listen for changes on each input checkbox tag
+  $('input[type=checkbox]').change(function () {
+    // Check if the checkbox is checked
+    if ($(this).prop('checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenities[$(this).attr('data-id')];
+    }
+    // Update the h4 tag inside the div Amenities with the list of Amenities checked
+    if (Object.keys(amenities).length === 0) {
+        $('div.amenities h4').html('&nbsp');
+    } else {
+      $('div.amenities h4').text(Object.values(amenities).join(', '));
+    }
+  });
+  // Request the API status every 5 seconds
+});
